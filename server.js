7291c8aff6490a778fab4d00e4d3a9365aa41f43
salem <Serhiy30131@gmail.com>
2025-02-09 05:51:35 +0200

    Main page

--- server.js
+++ server.js
@@ -12,38 +12,8 @@ app.use('/styles', express.static(path.join(__dirname, 'views/styles')));
 app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
-    const host = req.get('host');
-    const exampleData = {
-        title: "rockPi 4b state",
-        uptime: "6 days, 23:03",
-        dataSource: [
-            {
-                name: "CPU",
-                usedPercent: "3"
-            },
-            {
-                name: "RAM",
-                total: "3788.8MB",
-                used: "1228.8MB",
-                left: "2560.0MB",
-                usedPercent: "32"
-            },
-            {
-                name: "Disk",
-                total: "57.4G",
-                used: "8.9G",
-                left: "46.2G",
-                usedPercent: "16"
-            }
-        ]
-    };
-
-    const jsonString = JSON.stringify(exampleData);
-    
     const templateData = {
-        jsonData: JSON.stringify(exampleData, null, 2),
-        rawUrl: `http://${host}/api/v1/serverState?data=${jsonString}`,
-        encodedUrl: `http://${host}/api/v1/serverState?data=${encodeURIComponent(jsonString)}`
+        path: req.originalUrl
     };
 
     res.render('index', templateData);
